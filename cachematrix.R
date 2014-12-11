35930976b3422223b4187a5e90bda056debc6b38
shaojianab <shaojianab@qq.com>
2014-12-11 09:18:10 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## My functions will find the inversed matrix firstly, if it is not find,
+## then it will calculate a inversed matrix for the given matrix, and save it
+##in cache.
 
-## Write a short comment describing this function
+## This function create a matrix that contain four functions for the
+##given matrix just like the example.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  return (list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve))
+  
 }
 
 
-## Write a short comment describing this function
+## This function will find the x's inversed matrix, if not found in the cache,
+##it will calculate the inveresed matrix, and save it in cache.
 
 cacheSolve <- function(x, ...) {
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  return (m)
         ## Return a matrix that is the inverse of 'x'
 }
